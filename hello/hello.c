7d194c825537fa7a06273dff069fd83b72ceb787
a1115547 <a1115547@mail.nuk.edu.tw>
2025-04-12 17:58:42 +0800

    hello.c

--- hello/hello.c
+++ hello/hello.c
@@ -1,17 +1,50 @@
 #include <linux/init.h>
-#include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/proc_fs.h>
+#include <linux/uaccess.h>
+
+#define BUFFER_SIZE 128
+#define PROC_NAME "hello"
 
 MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("/proc Hello Module");
+MODULE_AUTHOR("A1115547");
 
-static int hello_init(void) {
-    printk(KERN_ALERT "Hello World A1115547\n");
+ssize_t proc_read(struct file* file, char __user* usr_buf, size_t count, loff_t* pos);
+
+static struct file_operations proc_ops = {
+    .owner = THIS_MODULE,
+    .read = proc_read,
+};
+
+int proc_init(void) {
+    proc_create(PROC_NAME, 0666, NULL, &proc_ops);
     return 0;
 }
 
-static void hello_exit(void) {
-    printk(KERN_ALERT "Goodbye World A1115547\n");
+void proc_exit(void) {
+    remove_proc_entry(PROC_NAME, NULL);
 }
 
-module_init(hello_init);
-module_exit(hello_exit);
+ssize_t proc_read(struct file* file, char __user* usr_buf, size_t count, loff_t* pos) {
+    int rv = 0;
+    char buffer[BUFFER_SIZE];
+    static int completed = 0;
+
+    if (completed) {
+        completed = 0;
+        return 0;
+    }
+
+    completed = 1;
+    rv = sprintf(buffer, "Hello World A1115547\n");
+    copy_to_user(usr_buf, buffer, rv);
+
+    return rv;
+}
+
+module_init(proc_init);
+module_exit(proc_exit);
+
+
